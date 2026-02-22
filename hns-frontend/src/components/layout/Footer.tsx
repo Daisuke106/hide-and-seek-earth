850985026ac17f155e72ee74c3741b10827462c3
Daisuke106 <daisuke.mtc@gmail.com>
2026-02-22 10:29:58 +0900

    フッターコンポーネントを新規作成し、スタイルを適用。地理探索ゲームの説明を追加。

---
+++ hns-frontend/src/components/layout/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Footer.css';
+
+export const Footer: React.FC = () => {
+  return (
+    <footer className="App-footer">
+      <p>Google Maps APIを使用した地理探索ゲームのプロトタイプ</p>
+      <p>キャラクターを探して、世界を冒険しよう！</p>
+    </footer>
+  );
+};
